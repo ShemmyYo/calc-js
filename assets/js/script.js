ad16fd37e92d8e05d3402d1b3b236c8a9874f40e
ShemmyYo <rabiliamemo@gmail.com>
2023-01-25 20:00:09 +0000

    number and operation buttons linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,47 +3,63 @@ const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equal]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
-const previousOperandTextElement = document.querySelector("[previous-oprand]");
-const currentOperandTextElement = document.querySelector("[current-operand]");
+const previousOperandTextElement = document.querySelector("[data-previous-operand]");
+const currentOperandTextElement = document.querySelector("[data-current-operand]");
 
-class Calcultor {
+class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-    }
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    };
 
     clear() {
-        this.curretntOperand = ""
-        this.previousOperand = ""
-        this.operation = undefined
-    }
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    };
 
     delete() {
 
-    }
+    };
 
-    appendNumber() {
+    appendNumber(number) {
+        if (number === "." && this.currentOperand.includes(".")) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    };
 
-    }
-
-    choseOperation() {
-
-    }
+    chooseOperation(operation) {
+        if (this.currentOperand === "") return;
+        if (this.previousOperand != "") {
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";     
+    };
 
     compute() {
 
-    }
+    };
 
-    updateDisplay() {
-
-    }
+    updateDisplay(number) {
+        this.currentOperandTextElement.innerHTML = this.currentOperand;
+        this.previousOperandTextElement.innerHTML = this.previousOperand;
+    };
 }
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+const calc = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
-    })
+        calc.appendNumber(button.innerText);
+        calc.updateDisplay();
+    });
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calc.chooseOperation(button.innerText);
+        calc.updateDisplay();
+    });
 })
